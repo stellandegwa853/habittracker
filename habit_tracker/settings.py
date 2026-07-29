d7879d1ef556a372632706a6bfba7c4fff977f47
WilliamGathii <billgathii@gmail.com>
2026-07-29 13:24:56 +0300

    Allow local Vite origins for Django CORS

--- habit_tracker/settings.py
+++ habit_tracker/settings.py
@@ -26,7 +26,10 @@ SECRET_KEY = 'django-insecure-wqk9z9gv9g6#&dfo(cv3d#k1e=vwl0xad177pydjz6c9v$%cu)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+]
 
 
 # Application definition
@@ -131,7 +134,16 @@ STATIC_URL = 'static/'
 # CORS settings
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
+    "http://localhost:5174",
     "http://127.0.0.1:5173",
+    "http://127.0.0.1:5174",
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    "http://localhost:5173",
+    "http://localhost:5174",
+    "http://127.0.0.1:5173",
+    "http://127.0.0.1:5174",
 ]
 
 # JWT Settings
