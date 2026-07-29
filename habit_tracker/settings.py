b68a60cd3f6386c5378c7e22cd78c23d4f0e17f1
WilliamGathii <billgathii@gmail.com>
2026-07-29 13:44:16 +0300

    Allow fallback Vite dev port

--- habit_tracker/settings.py
+++ habit_tracker/settings.py
@@ -135,15 +135,19 @@ STATIC_URL = 'static/'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://localhost:5174",
+    "http://localhost:5175",
     "http://127.0.0.1:5173",
     "http://127.0.0.1:5174",
+    "http://127.0.0.1:5175",
 ]
 
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:5173",
     "http://localhost:5174",
+    "http://localhost:5175",
     "http://127.0.0.1:5173",
     "http://127.0.0.1:5174",
+    "http://127.0.0.1:5175",
 ]
 
 # JWT Settings
